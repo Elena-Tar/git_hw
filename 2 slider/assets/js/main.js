56f980e949771837f0f5488c3fe2c1d4423b0c71
Elena <elena.taratina91@gmail.com>
2021-05-31 15:52:09 +0300

    33

--- 2 slider/assets/js/main.js
+++ 2 slider/assets/js/main.js
@@ -1,34 +1,34 @@
-// $(document).ready(function () {
-//     $('#imageGallery').lightSlider({
-//         gallery: true,
-//         item: 1,
-//         loop: true,
-//         thumbItem: 8,
-//         slideMargin: 0,
-//         enableDrag: false,
-//         currentPagerPosition: 'left',
-//         onSliderLoad: function (el) {
-//             lightGallery(el[0], { selector: '#imageGallery .lslide' });
-//         }
-//     });
-// });
-
-
 $(document).ready(function () {
     $('#imageGallery').lightSlider({
         gallery: true,
         item: 1,
         loop: true,
-        thumbItem: 9,
+        thumbItem: 8,
         slideMargin: 0,
         enableDrag: false,
         currentPagerPosition: 'left',
         onSliderLoad: function (el) {
-            el.lightGallery({
-                selector: '#imageGallery .lslide'
-            });
+            lightGallery(el[0], { selector: '#imageGallery .lslide' });
         }
     });
 });
+
+
+// $(document).ready(function () {
+//     $('#imageGallery').lightSlider({
+//         gallery: true,
+//         item: 1,
+//         loop: true,
+//         thumbItem: 9,
+//         slideMargin: 0,
+//         enableDrag: false,
+//         currentPagerPosition: 'left',
+//         onSliderLoad: function (el) {
+//             el.lightGallery({
+//                 selector: '#imageGallery .lslide'
+//             });
+//         }
+//     });
+// });
 
 
