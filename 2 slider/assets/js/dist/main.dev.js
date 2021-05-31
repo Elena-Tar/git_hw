de59ea0f2cae14dc236a176ff5a301cd37a330f6
Elena <elena.taratina91@gmail.com>
2021-05-31 15:49:13 +0300

    33 slider

--- 2 slider/assets/js/dist/main.dev.js
+++ 2 slider/assets/js/dist/main.dev.js
@@ -1,32 +1,17 @@
 "use strict";
 
-// $(function () {
-//     const slider = $('.slider').lightSlider({
-//         // item: 2,
-//         // auto: true,
-//         controls: false,
-//         loop: true, //листается закольцовано
-//         responsive: [
-//             {
-//                 breakpoint: 600,
-//                 settings: {
-//                     item: 2
-//                 }
-//             },
-//             {
-//                 breakpoint: 510,
-//                 settings: {
-//                     item: 1
-//                 }
-//             }
-//         ]
-//     });
-//     //сначала весь слайдер ьерем в какую-то переменную
-//     $('#slider_prev').on('click', function () {
-//         slider.goToPrevSlide()
-//     });
-//     $('#slider_next').on('click', function () {
-//         slider.goToNextSlide()
+// $(document).ready(function () {
+//     $('#imageGallery').lightSlider({
+//         gallery: true,
+//         item: 1,
+//         loop: true,
+//         thumbItem: 8,
+//         slideMargin: 0,
+//         enableDrag: false,
+//         currentPagerPosition: 'left',
+//         onSliderLoad: function (el) {
+//             lightGallery(el[0], { selector: '#imageGallery .lslide' });
+//         }
 //     });
 // });
 $(document).ready(function () {
@@ -34,7 +19,7 @@ $(document).ready(function () {
     gallery: true,
     item: 1,
     loop: true,
-    thumbItem: 8,
+    thumbItem: 9,
     slideMargin: 0,
     enableDrag: false,
     currentPagerPosition: 'left',
