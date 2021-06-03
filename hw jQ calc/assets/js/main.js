7e36ac4e40a98ba1a9d34f6907df4c65ca594df4
Elena <elena.taratina91@gmail.com>
2021-06-03 11:47:29 +0300

    add lesson 32 jQ

---
+++ hw jQ calc/assets/js/main.js
@@ -0,0 +1,31 @@
+$('.btn').on('click', function () {
+    $('.calc_screen').text($('.calc_screen').text() + this.value);
+})
+
+
+$(".btn_clear").on('click', function () {
+    $(".calc_screen").text('');
+});
+
+$('.btn_back').on('click', function () {
+    let res = $('.calc_screen').text();
+    $('.calc_screen').text(res.substring(0, res.length - 1));
+});
+
+$('.btn_equally').on('click', function () {
+    // debugger;
+    let a = $('.calc_screen').text();
+    console.log(a);
+    $('.calc_screen').text(eval(a));
+
+})
+
+$('.btn_comma').on('click', function () {
+    // debugger;
+    let x = $('.calc_screen').text();
+    if (x.indexOf(",") > -1) {
+        $('.calc_screen').text();
+    } else {
+        $('.calc_screen').text($('.calc_screen').text() + this.value);
+    }
+})
---
+++ hw jQ tab/assets/js/main.js
@@ -0,0 +1,75 @@
+// $(".tabs_list a").on('click', function (e) {
+//     e.preventDefault();
+//     if ($(this).hasClass("active") === false) {
+//         $(".tabs_list a.active").removeClass("active");
+//         $(this).addClass("active");
+//         $(".tab.active").removeClass("active");
+//         $($(this).attr("href") + '_tab').addClass("active");
+//     }
+// });
+
+
+$('.tabs').on('click', '.tab', function () {
+    // $(".tabs .tab").removeClass("active");
+    $(".tabs").find(".active").removeClass("active");
+
+    console.log($(this).index());
+    $(this).addClass('active');
+
+    /*метод .eq() выбирает элемент с конкретным индексом из набора выбранных элементов*/
+    // $('.tabs_block .content').eq($(this).index()).addClass("active");
+
+    $(".tabs_block").find(".active").removeClass("active");
+    ($('.tabs_block .content').eq($(this).index())).addClass("active");
+
+
+    // console.log(($('.tabs_block .content').eq($(this).index())));
+
+})
+
+
+
+
+/*star плагин*/
+$(".my-rating").starRating({
+    starSize: 24,
+    initialRating: 4,
+    useFullStars: true,
+    emptyColor: '#4b536b',
+    hoverColor: '#ffdd3f',
+    ratedColor: '#ffdd3f',
+    starShape: 'rounded',
+
+    callback: function (currentRating, $el) {
+
+    }
+});
+
+
+
+/**плагин кружка*/
+$(function () {
+    $('.chart').easyPieChart({
+        barColor: '#1ae681',  //  нужен градиенд это цвестастое
+        trackColor: '#787c9b', // серый кусок
+        // scaleColor: '#dfe0e0',
+        scaleLength: 0,  //палочки по бокам
+        lineCap: 'round',
+        lineWidth: 5,
+        // trackWidth: undefined,
+        trackWidth: 2,
+        size: 74,
+        rotate: 0,// in degrees
+        animate: {
+            duration: 1000,
+            enabled: true
+        },
+        easing: function (x, t, b, c, d) {// easing function
+            t = t / (d / 2);
+            if (t < 1) {
+                return c / 2 * t * t + b;
+            }
+            return -c / 2 * ((--t) * (t - 2) - 1) + b;
+        }
+    });
+});
